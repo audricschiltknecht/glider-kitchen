dcee0cb95a1cd79bcb5f3341f4d8770a7deaf7a4
Audric Schiltknecht <storm+github@chemicalstorm.org>
2025-07-31 13:50:13 -0400

    WIP Android
    # Conflicts:
    #	Cargo.lock
    #	glider-kitchen/Cargo.toml

--- glider-kitchen/src/lib.rs
+++ glider-kitchen/src/lib.rs
@@ -117,6 +117,26 @@ impl KitchenApp {
     }
 }
 
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: winit::platform::android::activity::AndroidApp) {
+    // Log to android output
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
+    );
+
+    let options = eframe::NativeOptions {
+        android_app: Some(app),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc)))),
+    )
+        .unwrap()
+}
+
 impl eframe::App for KitchenApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
